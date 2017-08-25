ca71cfd1e7ddede32c88960a6d405f5112c415f3
gaoguangxiao <gaoguangxiao125@sina.com>
2017-08-25 10:59:51 +0800

    选择题

--- HWExercises/Me/Controller/HWMeVC.h
+++ HWExercises/Me/Controller/HWMeVC.h
@@ -1,15 +1,13 @@
 //
-//  ViewController.h
+//  HWMeVC.h
 //  HWExercises
 //
 //  Created by sxmaps_w on 2017/5/22.
 //  Copyright © 2017年 wqb. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import "HWBaseViewController.h"
 
-@interface ViewController : UIViewController
-
+@interface HWMeVC : HWBaseViewController
 
 @end
-
